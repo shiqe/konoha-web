bc799d48fabf124b929018736795e8831dd6f46e
psycoder01 <aakashchy13@gmail.com>
2021-03-11 23:57:19 +0545

    back button added

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,7 +1,9 @@
 import React, { ReactElement, SyntheticEvent, useState } from 'react';
 
 import { Input } from 'baseui/input';
+import { Button } from 'baseui/button';
 import styled from 'styled-components';
+import { ArrowLeft } from 'baseui/icon';
 
 import { size } from '../../utils/spacing';
 import { signUpUser } from '../../api';
@@ -9,13 +11,18 @@ import PrimaryButton from '../Generic/PrimaryButton';
 import { FlexContainer, Title } from '../Generic/styled';
 import SnackBar from '../Snackbar';
 
+interface SignUpProps {
+    handleBackPress: () => void;
+}
+
 const initialCredentials = {
     name: '',
     email: '',
     password: '',
 };
 
-export default function SignUp(): ReactElement | null {
+export default function SignUp(props: SignUpProps): ReactElement | null {
+    const { handleBackPress } = props;
     const [snackBarMsg, setSnackbarMsg] = useState('');
     const [openSnackbar, setSnackbar] = useState(false);
     const [userCredentials, setUserCredentials] = useState(initialCredentials);
@@ -49,7 +56,18 @@ export default function SignUp(): ReactElement | null {
     return (
         <Container>
             <FlexContainer flexDirection="column" gap={size.lg}>
-                <Title fontSize={size.x2l}>Sign Up</Title>
+                <FlexContainer
+                    flexDirection="row"
+                    justify="space-between"
+                    gap={size.sm}
+                    style={{ width: '100%' }}
+                >
+                    <Button onClick={handleBackPress}>
+                        <ArrowLeft size={size.xl} />
+                    </Button>
+                    <Title fontSize={size.x2l}>Sign Up</Title>
+                    <div />
+                </FlexContainer>
                 <form>
                     <FlexContainer flexDirection="column" gap={size.lg}>
                         <Input
